3284688b7e6e562382126076e023eaeeabd216f4
Xray1221 <105057432+Xray1221@users.noreply.github.com>
2022-09-23 13:13:07 +0300

    complete

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createImageCardsMarkup(galleryItems);
+console.log(galleryMarkup);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createImageCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" 
+      href="${original}">
+      <img class="gallery__image" 
+      src="${preview}" 
+      title="${description}" />
+    </a>`;
+    })
+    .join("");
+}
+const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
